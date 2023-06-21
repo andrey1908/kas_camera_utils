52007cde5d36df431250eebbd2e6489b827f3bb9
andrey1908 <andrey.krishtopik@mail.ru>
2023-06-21 10:44:31 +0300

    realsense_camera.py: rename enable_color -> enable_image; read camera intrinsic parameters in RealsenseCamera.__init__() function; minor changes in functions to read frames

--- realsense_camera.py
+++ realsense_camera.py
@@ -3,20 +3,36 @@ import numpy as np
 
 
 class RealsenseCamera:
-    def __init__(self, enable_color=True, enable_depth=False, align_depth2color=True):
-        self.enable_color = enable_color
+    def __init__(self, enable_image=True, enable_depth=False, align_depth2image=True):
+        self.enable_image = enable_image
         self.enable_depth = enable_depth
-        self.align_depth2color = align_depth2color
+        self.align_depth2image = align_depth2image
 
         self.config = rs.config()
-        if self.enable_color:
+        if self.enable_image:
             self.config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
         if self.enable_depth:
             self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
         self.align = None
-        if self.enable_depth and self.align_depth2color:
+        if self.enable_depth and self.align_depth2image:
             self.align = rs.align(rs.stream.color)
         self.pipeline = rs.pipeline()
+
+        self.start()
+        profile = self.pipeline.get_active_profile()
+        if self.enable_image:
+            color_stream = profile.get_stream(rs.stream.color)
+            intrinsics = color_stream.as_video_stream_profile().get_intrinsics()
+            self.K = np.eye(3)
+            self.K[0, 0] = intrinsics.fx
+            self.K[1, 1] = intrinsics.fy
+            self.K[0, 2] = intrinsics.ppx
+            self.K[1, 2] = intrinsics.ppy
+            self.D = np.array(intrinsics.coeffs)
+        if self.enable_depth:
+            depth_sensor = profile.get_device().first_depth_sensor()
+            self.depth_scale = depth_sensor.get_depth_scale()
+        self.stop()
 
     def start(self):
         self.pipeline.start(self.config)
@@ -25,31 +41,30 @@ class RealsenseCamera:
         self.pipeline.stop()
 
     def __call__(self):
-        if self.enable_depth:
-            image, depth = self.read(read_depth=True)
-            camera_frames = {"image": image, "depth": depth}
-        else:
-            image = self.read()
-            camera_frames = {"image": image}
-        return camera_frames
+        frames = dict()
+        frames_list = self.read(read_image=True, read_depth=True)
+        for frame, name in zip(frames_list, ("image", "depth")):
+            if frame is not None:
+                frames[name] = frame
+        return frames
 
-    def read(self, read_color=True, read_depth=False):
+    def read(self, read_image=True, read_depth=False):
         frames = self.pipeline.wait_for_frames()
         if self.align:
             frames = self.align.process(frames)
 
         ret = list()
-        if read_color:
-            color_frame = frames.get_color_frame()
-            if color_frame:
+        if read_image:
+            if self.enable_image:
+                color_frame = frames.get_color_frame()
                 image = np.array(color_frame.get_data())
                 ret.append(image)
             else:
                 ret.append(None)
 
         if read_depth:
-            depth_frame = frames.get_depth_frame()
-            if depth_frame:
+            if self.enable_depth:
+                depth_frame = frames.get_depth_frame()
                 depth = np.array(depth_frame.get_data())
                 ret.append(depth)
             else:
@@ -69,29 +84,3 @@ class RealsenseCamera:
         profile = self.pipeline.get_active_profile()
         color_sensor = profile.get_device().first_color_sensor()
         color_sensor.set_option(rs.option.exposure, exposure)
-
-    def get_depth_scale(self):
-        profile = self.pipeline.get_active_profile()
-        depth_sensor = profile.get_device().first_depth_sensor()
-        depth_scale = depth_sensor.get_depth_scale()
-        return depth_scale
-
-    def get_intrinsics(self):
-        profile = self.pipeline.get_active_profile()
-        color_stream = profile.get_stream(rs.stream.color)
-        intrinsics = color_stream.as_video_stream_profile().get_intrinsics()
-        return intrinsics
-
-    def get_K(self):
-        intrinsics = self.get_intrinsics()
-        K = np.eye(3)
-        K[0, 0] = intrinsics.fx
-        K[1, 1] = intrinsics.fy
-        K[0, 2] = intrinsics.ppx
-        K[1, 2] = intrinsics.ppy
-        return K
-
-    def get_D(self):
-        intrinsics = self.get_intrinsics()
-        D = np.array(intrinsics.coeffs)
-        return D
