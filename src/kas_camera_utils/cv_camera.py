dc0f3233b654c9a2a4463845ab5985165d7891c8
andrey1908 <andrey.krishtopik@mail.ru>
2023-08-16 05:30:05 +0300

    add cv_camera.py

---
+++ src/kas_camera_utils/cv_camera.py
@@ -0,0 +1,46 @@
+import cv2
+
+
+class CvCamera:
+    def __init__(self, source):
+        self.source = source
+
+        self.video = cv2.VideoCapture()
+
+        if isinstance(self.source, str):
+            self.video.open(self.source)
+            if not self.video.isOpened():
+                raise FileNotFoundError(f"Video file not found: {self.source}")
+
+    def is_active(self):
+        return self.video.isOpened()
+
+    def start(self):
+        if not isinstance(self.source, str):
+            self.video.open(self.source)
+            if not self.video.isOpened():
+                raise RuntimeError(f"Can't open video device {self.source}")
+
+    def try_start(self):
+        if not self.is_active():
+            self.start()
+
+    def stop(self):
+        if not isinstance(self.source, str):
+            self.video.release()
+
+    def __call__(self):
+        image = self.read()
+        return {"image": image}
+
+    def read(self):
+        ret, image = self.video.read()
+        if not ret:
+            if isinstance(self.source, str):
+                self.video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                ret, image = self.video.read()
+                if not ret:
+                    raise RuntimeError("Could not restart video")
+            else:
+                raise RuntimeError(f"Could not read from video device {self.source}")
+        return image
